d467bf273ff3eec01ffb13885a36009139845944
Unai Castro <ucastro@purpleblob.net>
2024-04-30 15:34:31 +0200

    INtento de progressbar para aumentar cantidades modelo

--- app/src/main/java/com/tfg/smartdiet/iu/PaginaPrincipal/PrimeraPantalla/PrincipalFragment.kt
+++ app/src/main/java/com/tfg/smartdiet/iu/PaginaPrincipal/PrimeraPantalla/PrincipalFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import com.tfg.smartdiet.R
 import com.tfg.smartdiet.databinding.FragmentPrincipalBinding
@@ -13,14 +14,47 @@ class PrincipalFragment : Fragment() {
 
     private var _binding: FragmentPrincipalBinding? = null
     private val binding get() = _binding!!
+    private var calorias=15
+    private var MAX_CALORIAS_DIARIAS=100.0f
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentPrincipalBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.BotonB.setOnClickListener {
+            calorias += 45
+            initUI()
+        }
+    }
+
+    private fun initUI() {
+        // Obtén una referencia al ProgressBar
+        val progressBar: ProgressBar = binding.progressBar
+
+        // Supongamos que tienes una variable que almacena las calorías consumidas
+        val caloriasConsumidas: Int = obtenerCaloriasConsumidas()
+
+        // Calcula el progreso como un porcentaje
+        val progreso: Int = (caloriasConsumidas / MAX_CALORIAS_DIARIAS.toFloat() * 100).toInt()
+
+        // Actualiza el progreso del ProgressBar
+        progressBar.progress = progreso
+    }
+
+    private fun obtenerCaloriasConsumidas(): Int {
+        return this.calorias
     }
 
 
